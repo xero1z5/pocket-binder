87ba9862f305925df0cebcee469bee511da042de
xero <kunalcode05@gmail.com>
2026-06-09 19:18:49 +0530

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,8 @@ fn App() -> Element {
     // 3. UI LAYOUT
     // =========================================================================
     rsx! {
+        document::Meta { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" }
+
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS } 
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
